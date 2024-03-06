e9fa336886a38b2aca36245695bf1edee40bfae3
RupalGoyal <79362422+RupalGoyal@users.noreply.github.com>
2024-03-06 19:55:10 +0530

    Add files via upload
    first commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,63 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Modal Enhancement for Breaking News",
+        tags:["react","gsap","styled-components"],
+        date:"13 May, 2021",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        description:"User-Centric Design: Implemented a comprehensive history view with a keen focus on enhancing the overall user experience, ensuring easy navigation and accessibility for breaking news updates. Authoring Empowerment: Introduced authoring capabilities that streamlined the content creation process, allowing for efficient generation and reopening of previous entries, contributing to editorial efficiency. "
+    },
+    {
+        id:2,
+        name:"How to create React Hamburger Menu using Styled-Components",
+        tags:["react","styled-components"],
+        date:"5 July, 2021",
+        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        description:""
+    },
+    // {
+    //     id:3,
+    //     name:"React Loading Screen: Try these 3 cool loading screens for your app",
+    //     tags:["react","react-lottie","styled-components"],
+    //     date:"6 July, 2021",
+    //     imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625552344293%2Fegwis0UIX.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+    //     description:"https://codebucks.hashnode.dev/react-loading-screens"
+    // },
+    {
+        id:5,
+        name:"AI-Driven Chat Bot Project",
+        tags:["react","reactJS","web-development"],
+        date:"3 Feb, 2021",
+        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        description:"Advanced AI Integration: Spearheaded the development of a sophisticated chatbot by seamlessly integrating advanced AI/ML services. Natural Language Processing (NLP): Implemented NLP algorithms within the chatbot, enabling it to understand and respond to user queries in a more human-like manner, fostering a personalized and engaging interaction."
+    },
+    {
+        id:6,
+        name:"User Data and Analytics Tracking Service:",
+        tags:["react","css"],
+        date:"5 Jan, 2021",
+        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        description:"Comprehensive Data Insights: Designed and implemented a robust tracking service that provided comprehensive insights into user behavior and interactions. Scalability: Engineered the tracking service with scalability in mind, ensuring it seamlessly accommodated the growing user base and evolving website structure. "
+
+    },
+   
+    // {
+    //     id:7,
+    //     name:"What is Redux ? Simply Explained!",
+    //     tags:["react","redux","javascript"],
+    //     date:"16 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
+    //     link:"https://dev.to/codebucks/what-is-redux-simply-explained-2ch7"
+    // },
+    // {
+    //     id:8,
+    //     name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+    //     tags:["react","framer-motion","styled-components"],
+    //     date:"26 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+    //     link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
+    // },
+  
+] 
